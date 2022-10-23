84310908f38dd6d70d2208904edb2750e7ba1c1c
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-10-23 11:29:57 -0500

    lint test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,7 @@ const HomePage: NextPage<HomePageProps> = (props) => {
 
   return (
     <HomeContext.Provider value={{ artist }}>
-      <Home />
-    </HomeContext.Provider>
+      <Home /></HomeContext.Provider>
   );
 };
 
@@ -52,7 +51,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       limit,
       seed_artists
     }
-  );
+  )
 
   const { tracks }: { tracks: ITrack[] } = await fetchWithToken(
     req as NextApiRequest,
