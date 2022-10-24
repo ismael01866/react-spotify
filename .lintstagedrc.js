093173c84fab9b016e55b4624854808d327f8c31
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-10-24 07:49:41 -0500

    lint test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `next lint --cache --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+// const buildEslintCommand = (filenames) =>
+//   `next lint --cache --fix --file ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(' --file ')}`;
 
-const buildPrettierCommand = (filenames) =>
-  `prettier --config .prettierrc --write ${filenames.join(' ')}`;
+// const buildPrettierCommand = (filenames) =>
+//   `prettier --config .prettierrc --write ${filenames.join(' ')}`;
 
-const buildStyleLintCommand = (filenames) =>
-  `stylelint --syntax scss --fix ${filenames.join(' ')}`;
+const buildStyleLintCommand = (filenames) => {
+  console.log(filenames.join(' '));
+  return `stylelint --fix ${filenames.join(' ')}`;
+};
 
 module.exports = {
-  '*.{ts, tsx}': 'tsc --project tsconfig.json --pretty --noEmit',
-  '*.{js, jsx, ts, tsx}': [buildEslintCommand],
-  '*.{css, scss}, *.module.scss': [buildStyleLintCommand],
-  '*.{js, jsx, ts, tsx, css, scss, md, json}': [buildPrettierCommand]
+  // '*.{ts, tsx}': 'tsc --project tsconfig.json --pretty --noEmit',
+  // '*.{js, jsx, ts, tsx}': [buildEslintCommand],
+  '*.{css, scss}, *.module.scss': [buildStyleLintCommand]
+  // '*.{js, jsx, ts, tsx, css, scss, md, json}': [buildPrettierCommand]
 };
