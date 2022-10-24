66aef74e9acf73140aede2d811f11a5426f34433
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-10-23 19:11:13 -0500

    update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,13 +5,12 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
-const buildTSCommand = () => `npm run type-check`;
-
 const buildPrettierCommand = (filenames) =>
   `prettier --config .prettierrc --write ${filenames.join(' ')}`;
 
 module.exports = {
-  '*.{ts, tsx}': [buildTSCommand],
+  '*.{ts, tsx}': 'npm run type-check',
   '*.{js, jsx, ts, tsx}': [buildEslintCommand],
+  '*.{css, scss}': 'stylelint --fix',
   '*.{js, jsx, ts, tsx, css, scss, md, json}': [buildPrettierCommand]
 };
