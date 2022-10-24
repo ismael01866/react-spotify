440e3ca76369c4946bce9b5cc0164c363907e2ca
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-10-24 08:11:07 -0500

    lint test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
+const buildEsLintCommand = (filenames) =>
   `next lint --cache --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
-const buildPrettierCommand = (filenames) =>
-  `prettier --config .prettierrc --write ${filenames.join(' ')}`;
-
 const buildStyleLintCommand = (filenames) =>
   `stylelint --fix ${filenames.join(' ')}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --config .prettierrc --write ${filenames.join(' ')}`;
+
+const buildTSCCommand = () => `tsc --noEmit`;
+
 module.exports = {
-  // '*.{ts, tsx}': 'tsc --project tsconfig.json --pretty --noEmit',
+  '*.{ts,tsx}': [buildTSCCommand],
   '*.{js,jsx,ts,tsx,css,scss,md,json}': [buildPrettierCommand],
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEsLintCommand],
   '*.{css,scss}': [buildStyleLintCommand]
 };
