7fac2f922c4fd9daa994f79665b0dd03bbb8a00f
Ismael Mariscal <ismael.valladares@cymatic.io>
2022-10-24 08:12:16 -0500

    lint test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -11,7 +11,7 @@ const buildStyleLintCommand = (filenames) =>
 const buildPrettierCommand = (filenames) =>
   `prettier --config .prettierrc --write ${filenames.join(' ')}`;
 
-const buildTSCCommand = () => `tsc --noEmit`;
+const buildTSCCommand = () => `tsc --project tsconfig.json --noEmit`;
 
 module.exports = {
   '*.{ts,tsx}': [buildTSCCommand],
